492098338ce82a471bcca29ea5b747e52d1843c5
Western8 <westernchrome@gmail.com>
2024-02-15 23:42:50 +0300

    feat: add loader

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -2,6 +2,7 @@ import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema, gqlSchema } from './schemas.js';
 import { graphql, parse, validate } from 'graphql';
 import depthLimit from 'graphql-depth-limit';
+import DataLoader from 'dataloader';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   const { prisma } = fastify;
@@ -18,8 +19,8 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
     async handler(req) {
       //console.log('зашли в handler graphql !!!!!!!')
       const errors = validate(gqlSchema, parse(req.body.query), [depthLimit(5)]);
-      if (errors.length) return { errors }; 
-      
+      if (errors.length) return { errors };
+
       const result = graphql({
         schema: gqlSchema,
         source: req.body.query,
@@ -32,12 +33,134 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
     },
   });
 
+  type TUsers = { id: string; name: string; balance: number; }
+  //const loaders: DataLoader<unknown, TUsers[]>[] = [];
+
+  const loaderUsers = new DataLoader(async (ids: readonly string[]) => {
+    const users = await prisma.user.findMany({
+      where: {
+        id: { in: ids as string[] }
+      }
+    });
+    const usersSorted = ids.map(id => users.filter(item => item.id === id));
+    return usersSorted;
+  });
+
+  const loaderProfiles = new DataLoader(async (ids: readonly string[]) => {
+    const profiles = await prisma.profile.findMany({
+      where: {
+        userId: { in: ids as string[] }
+      }
+    });
+    const profilesSorted = ids.map(id => profiles.filter(item => item.userId === id));
+    return profilesSorted;
+  });
+
+  const loaderPosts = new DataLoader(async (ids: readonly string[]) => {
+    const posts = await prisma.post.findMany({
+      where: {
+        authorId: { in: ids as string[] }
+      }
+    });
+    const postsSorted = ids.map(id => posts.filter(item => item.authorId === id));
+    return postsSorted;
+  });
+
+  const loaderMemberTypes = new DataLoader(async (ids: readonly string[]) => {
+    const memberTypes = await prisma.memberType.findMany({
+      where: {
+        id: { in: ids as string[] }
+      }
+    });
+    const memberTypesSorted = ids.map(id => memberTypes.filter(item => item.id === id));
+    return memberTypesSorted;
+  });
+
+  interface Sub {
+    subscriberId: string | undefined;
+    authorId: string | undefined;
+  };
+
+  const loaderSubsAll = new DataLoader(async (ids: readonly number[]) => {
+    const subscribersOnAuthors = await prisma.subscribersOnAuthors.findMany();
+    const subscribersOnAuthorsSorted = ids.map(id => subscribersOnAuthors);
+    return subscribersOnAuthorsSorted;
+  });
+
+  const loaderUsersAll = new DataLoader(async (ids: readonly number[]) => {
+    const users = await prisma.user.findMany();
+    const usersSorted = ids.map(id => users);
+    return usersSorted;
+  });
+
+  /*
+  const loaderSubs = new DataLoader(async (ids: readonly Sub[]) => {
+    //const idsSubscriber = ids.map(item => item.subscriberId);
+    //const idsAuthor = ids.map(item => item.authorId);
+    const subscribersOnAuthors = await prisma.subscribersOnAuthors.findMany();
+    /*
+    const subscribersOnAuthors = await prisma.subscribersOnAuthors.findMany({
+      where: {
+        OR: [
+          { subscriberId: { in: idsSubscriber as string[] } },
+          { authorId: { in: idsAuthor as string[] } },
+        ]
+      }
+    });
+    */
+    //const subscribersOnAuthorsSorted = ids.map(id => subscribersOnAuthors);
+    /*
+     const subscribersOnAuthorsSorted = ids.map(id => subscribersOnAuthors.filter(item => {
+       if (id.subscriberId) return item.subscriberId === id.subscriberId;
+       if (id.authorId) return item.authorId === id.authorId;
+       return true;
+     }));
+     */
+   // return subscribersOnAuthorsSorted;
+  //});
+  /*
+    const loaderSubsTo = new DataLoader(async (ids: readonly string[]) => {
+      const subscribersOnAuthors = await prisma.subscribersOnAuthors.findMany({
+        where: {
+          subscriberId: { in: ids as string[] }
+        }
+      });
+      const subscribersOnAuthorsSorted = ids.map(id => subscribersOnAuthors.filter(item => item.subscriberId === id));
+      return subscribersOnAuthorsSorted;
+    });
+  
+    const loaderSubsBack = new DataLoader(async (ids: readonly string[]) => {
+      const subscribersOnAuthors = await prisma.subscribersOnAuthors.findMany({
+        where: {
+          authorId: { in: ids as string[] }
+        }
+      });
+      const subscribersOnAuthorsSorted = ids.map(id => subscribersOnAuthors.filter(item => item.authorId === id));
+      return subscribersOnAuthorsSorted;
+    });
+  */
   const resolvers = {
     memberTypes: async () => await prisma.memberType.findMany(),
     posts: async () => await prisma.post.findMany(),
     users: async () => {
       const users = await prisma.user.findMany();
-      return users.map(async (item) => await getUserData(item));
+      //const users = loaderUsers.loadMany();
+      //return users.map(async (item) => await getUserData(item));
+      /*
+      if (!loaders[0]) {
+        console.log('Зашли в loader!!!!!!!!!! ');
+        const loader = new DataLoader(async ([k]) => {
+          return [await prisma.user.findMany()];
+        });
+        loaders.push(loader);
+        //loaders.loader = loader;
+      }
+
+      const users = await loaders[0].load(k);
+      */
+      return users.map(async (item, __, users) => await getUserData(item, users));
+      //return users.map((item) => getUserData(item));
+      //setTimeout(() => {}, 0);
     },
     profiles: async () => {
       const profiles = await prisma.profile.findMany();
@@ -53,9 +176,11 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       return result || null
     },
     user: async (args) => {
-      const user = await prisma.user.findUnique({ where: { id: args.id } })
+      //const user = await prisma.user.findUnique({ where: { id: args.id } })
+      const user = (await loaderUsers.load(args.id))[0];
       if (user) {
         return await getUserData(user);
+        //return await getUserData(user, loaderPosts);
       } else {
         return null;
       }
@@ -143,8 +268,120 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
     },
   }
 
+  let counter = 0;
+  const getUserData = async (user, users: TUsers[] = []) => {
+    const posts = await loaderPosts.load(user.id);
+    //const profile = await prisma.profile.findUnique({ where: { userId: user.id } });
+    const profiles = await loaderProfiles.load(user.id);
+    const profile = profiles[0];
+    const profileData = await getProfileData(profile);
+
+    let usersAll = users;
+    if (!users.length) {
+      usersAll = await loaderUsersAll.load(counter);
+    }
+    counter++;
+    const subsAll = await loaderSubsAll.load(counter);
+    //const subsAll = await loaderSubsAll.load(user.id);
+
+
+    const subs = await subsAll.filter(item => item.subscriberId === user.id );
+    //console.log('1111111111 subsAll ', subsAll);
+    //console.log('1111111111 subs ', subs);
+    
+    const userSubscribedTo = subs.map(async (item) => {
+      //const subTo = await prisma.user.findUnique({ where: { id: item.authorId } });
+      //const subTo = (await loaderUsers.load(item.authorId))[0];
+      const subTo = usersAll.find(itemUsersAll => itemUsersAll.id === item.authorId );;
+      if (!subTo) throw new Error('A нету юзера!!!!!!!!!!!!!!!!!');// return {};
+      //const subsBack = await prisma.subscribersOnAuthors.findMany({ where: { authorId: subTo.id } });
+      //const subsBack = await loaderSubsBack.load(subTo.id);
+      const subsBack = subsAll.filter(item => item.authorId === subTo.id );
+      const subscribedToUser = subsBack.map(itemSubBack => { return { id: itemSubBack.subscriberId } });
+      return {
+        id: subTo.id,
+        name: subTo.name,
+        subscribedToUser,
+      }
+    });
+
+
+    const subsBack = await  subsAll.filter(item => item.authorId === user.id );
+    const subscribedToUser = subsBack.map(async (item) => {
+      //const subBack = await prisma.user.findUnique({ where: { id: item.subscriberId } });
+      //const subBack = (await loaderUsers.load(item.subscriberId))[0];
+      const subBack = usersAll.find(itemUsersAll => itemUsersAll.id === item.subscriberId );;
+      if (!subBack) throw new Error('A нету юзера!!!!!!!!!!!!!!!!!');
+      //if (!subBack) return {};
+      //const subsTo = await prisma.subscribersOnAuthors.findMany({ where: { subscriberId: subBack.id } })
+      //const subsTo = await loaderSubsTo.load(subBack.id);
+      const subsTo = subsAll.filter(item => item.subscriberId === subBack.id );
+      const userSubscribedTo = subsTo.map(itemSubTo => { return { id: itemSubTo.authorId } });
+      return {
+        id: subBack.id,
+        name: subBack.name,
+        userSubscribedTo,
+      }
+    })
+
+
+    //loaderSubs.load({ subscriberId: user.id, authorId: undefined });
+    //loaderSubs.load({ subscriberId: undefined, authorId: user.id });
+
+    //const subs = await prisma.subscribersOnAuthors.findMany({ where: { subscriberId: user.id } })
+    //const subs = await loaderSubsTo.load(user.id);
+    /*
+    const subs =  await    loaderSubs.load({ subscriberId: user.id, authorId: undefined });
+    const userSubscribedTo = subs.map(async (item) => {
+      //const subTo = await prisma.user.findUnique({ where: { id: item.authorId } });
+      const subTo = (await loaderUsers.load(item.authorId))[0];
+      if (!subTo) return {};
+      //const subsBack = await prisma.subscribersOnAuthors.findMany({ where: { authorId: subTo.id } });
+      //const subsBack = await loaderSubsBack.load(subTo.id);
+      const subsBack =  await   loaderSubs.load({ subscriberId: undefined, authorId: subTo.id });
+      const subscribedToUser = subsBack.map(itemSubBack => { return { id: itemSubBack.subscriberId } });
+      return {
+        id: subTo.id,
+        name: subTo.name,
+        subscribedToUser,
+      }
+    })
+    */
+    //const subsBack = await prisma.subscribersOnAuthors.findMany({ where: { authorId: user.id } });
+    //const subsBack = await loaderSubsBack.load(user.id);
+    /*
+    const subsBack =    await  loaderSubs.load({ subscriberId: undefined, authorId: user.id });
+     const subscribedToUser = subsBack.map(async (item) => {
+       //const subBack = await prisma.user.findUnique({ where: { id: item.subscriberId } });
+       const subBack = (await loaderUsers.load(item.subscriberId))[0];
+       if (!subBack) return {};
+       //const subsTo = await prisma.subscribersOnAuthors.findMany({ where: { subscriberId: subBack.id } })
+       //const subsTo = await loaderSubsTo.load(subBack.id);
+       const subsTo =    await   loaderSubs.load({ subscriberId: subBack.id, authorId: undefined });
+       const userSubscribedTo = subsTo.map(itemSubTo => { return { id: itemSubTo.authorId } });
+       return {
+         id: subBack.id,
+         name: subBack.name,
+         userSubscribedTo,
+       }
+     })
+     */
+
+    return {
+      id: user.id,
+      name: user.name,
+      balance: user.balance,
+      profile: profileData,
+      posts,
+      userSubscribedTo: userSubscribedTo,
+      subscribedToUser: subscribedToUser,
+    }
+  }
+
+  /*
   const getUserData = async (user) => {
-    const posts = await prisma.post.findMany({ where: { authorId: user.id } });
+    //const posts = await prisma.post.findMany({ where: { authorId: user.id } });
+    const posts = await loaderPosts.load(user.id);
     const profile = await prisma.profile.findUnique({ where: { userId: user.id } });
     const profileData = await getProfileData(profile);
 
@@ -184,10 +421,11 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       subscribedToUser: subscribedToUser,
     }
   }
-
+*/
   const getProfileData = async (profile) => {
     if (profile) {
-      const memberType = await prisma.memberType.findUnique({ where: { id: profile.memberTypeId } })
+      //const memberType = await prisma.memberType.findUnique({ where: { id: profile.memberTypeId } })
+      const memberType = (await loaderMemberTypes.load(profile.memberTypeId))[0];
       return {
         id: profile.id,
         isMale: profile.isMale,
